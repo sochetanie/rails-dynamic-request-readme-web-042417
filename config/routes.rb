6f5b4af323cd68a3f835db64badf9ec267b0a817
sochetanie <allagorik2901@gmail.com>
2017-05-19 14:20:31 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/:id', to: 'posts#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
